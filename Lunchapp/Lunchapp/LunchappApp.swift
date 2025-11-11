76ffdd4bf4209a1bcf9aca7a03ea2a49ac1fc68b
saracrespo222 <saricrespo2@gmail.com>
2025-11-11 10:00:40 +0100

    LunchApp
    start

---
+++ Lunchapp/Lunchapp/LunchappApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchappApp.swift
+//  Lunchapp
+//
+//  Created by Sara Fernanda Crespo Galindo  on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LunchappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
